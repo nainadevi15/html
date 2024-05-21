6d871268dafd82e598a55db099b4e213d9ce52aa
nainadevi15 <146159321+nainadevi15@users.noreply.github.com>
2024-05-21 11:41:07 +0530

    Delete project2 directory

--- project2/src/assets/Layout.jsx
+++
@@ -1,9 +0,0 @@
- import Header,{Footer,Content} from "./Components.jsx"
- 
- export default function Layout(){
-    return <div>
-        <Header></Header>
-        <Content></Content>
-        <Footer></Footer>
-    </div>
-}
